ee476287df9a4fa0e0ad1cf58a5c05503115cbaa
MrEdvinVanCliff <slavik.fedorets@gmail.com>
2026-05-05 17:07:03 +0200

    new changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function observeAll() {
         io.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.35 });
+  }, { threshold: 0.20 });
 
   items.forEach((item) => io.observe(item));
 }
